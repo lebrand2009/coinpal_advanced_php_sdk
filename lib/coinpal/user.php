e7da0b084a052ef6e56d3b3c9c95da227b4bfcdb
Le Brand REAL IT Solutions <lebrand2009@hotmail.com>
2024-04-30 04:31:54 +0300

    Update user.php

--- lib/coinpal/user.php
+++ lib/coinpal/user.php
@@ -1,6 +1,6 @@
 <?php
 $user = array (
-  'username' => 'lebrand',
-  'password' => '$argon2id$v=19$m=65536,t=4,p=1$1UWcKuMHuJzqiXLmeyApqQ$+bmV4TptLWLZxaaK7jUkST15bXhs47NXVU63uoX5kN0',
+  'username' => '',
+  'password' => '',
 );
 ?>
